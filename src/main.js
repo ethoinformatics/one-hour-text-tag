cb8cbe9044433a9d20e71840fcebeb56959d9060
Mike Chevett <mchevett@gmail.com>
2014-10-03 17:12:11 -0400

    times up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('./main.less');
+
 var $ = window.$,
 	template = require('./main.vash');
 
@@ -5,8 +7,34 @@ $(function(){
 	var $body = $('body');
 	$body.append(template({}));
 
+	var $btn = $body.find('button[type=submit]'),
+		$modal = $('#myModal'),
+		$modalBody = $modal.find('.modal-body');
+
+	$btn.click(function(ev){
+		ev.preventDefault();
+
+		var findText = $body.find('#txtSearch').val(),
+			replaceText = $body.find('#txtReplace').val(),
+			documentText = $body.find('textarea').val();
+
+		var result = go(findText, replaceText, documentText);
+
+		$modalBody.html(result);
+		$modal.modal('show');
+	});
 
 
 });
+
+
+function go(find, replace, txt){
+
+	debugger
+
+	var a = new RegExp('\\b('+find +')\\b', 'ig');
+
+	return txt.replace(a, '<b>$1</b>');
+}
 
 
